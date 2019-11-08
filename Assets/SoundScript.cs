ea96d3cd0aec564ee538254d280b376ffbf577a6
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-11-08 11:52:08 +0100

    added pause

--- Assets/SoundScript.cs
+++ Assets/SoundScript.cs
@@ -7,6 +7,9 @@ public class SoundScript : MonoBehaviour
 {
     public AudioClip honk;
     AudioSource aS;
+
+    public AudioSource music;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -44,6 +47,48 @@ public class SoundScript : MonoBehaviour
             PlaySound(12);
         if (Input.GetKeyDown(KeyCode.Comma))
             PlaySound(13);
+        if (Input.GetKeyDown(KeyCode.Period))
+            PlaySound(14);
+
+        if (Input.GetKeyDown(KeyCode.O))
+            PlaySound(0);
+        if (Input.GetKeyDown(KeyCode.I))
+            PlaySound(1);
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+            PlaySound(2);
+        if (Input.GetKeyDown(KeyCode.D))
+            PlaySound(3);
+        if (Input.GetKeyDown(KeyCode.C))
+            PlaySound(4);
+        if (Input.GetKeyDown(KeyCode.V))
+            PlaySound(5);
+        if (Input.GetKeyDown(KeyCode.G))
+            PlaySound(6);
+        if (Input.GetKeyDown(KeyCode.B))
+            PlaySound(7);
+        if (Input.GetKeyDown(KeyCode.H))
+            PlaySound(8);
+        if (Input.GetKeyDown(KeyCode.N))
+            PlaySound(9);
+        if (Input.GetKeyDown(KeyCode.J))
+            PlaySound(10);
+        if (Input.GetKeyDown(KeyCode.M))
+            PlaySound(11);
+        if (Input.GetKeyDown(KeyCode.K))
+            PlaySound(12);
+        if (Input.GetKeyDown(KeyCode.Comma))
+            PlaySound(13);
+        if (Input.GetKeyDown(KeyCode.Period))
+            PlaySound(14);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (music.isPlaying)
+                music.Pause();
+            else
+                music.UnPause();
+        }
+       
     }
 
     void PlaySound(float steps)
