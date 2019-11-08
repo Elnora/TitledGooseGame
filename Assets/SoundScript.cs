7bff3d01a36653cb90e898232b6c163172cc8ff1
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-11-08 12:06:11 +0100

    bigger piano

--- Assets/SoundScript.cs
+++ Assets/SoundScript.cs
@@ -50,36 +50,34 @@ public class SoundScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Period))
             PlaySound(14);
 
-        if (Input.GetKeyDown(KeyCode.O))
-            PlaySound(0);
         if (Input.GetKeyDown(KeyCode.I))
-            PlaySound(1);
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-            PlaySound(2);
-        if (Input.GetKeyDown(KeyCode.D))
-            PlaySound(3);
-        if (Input.GetKeyDown(KeyCode.C))
-            PlaySound(4);
-        if (Input.GetKeyDown(KeyCode.V))
-            PlaySound(5);
-        if (Input.GetKeyDown(KeyCode.G))
-            PlaySound(6);
-        if (Input.GetKeyDown(KeyCode.B))
-            PlaySound(7);
-        if (Input.GetKeyDown(KeyCode.H))
-            PlaySound(8);
-        if (Input.GetKeyDown(KeyCode.N))
-            PlaySound(9);
-        if (Input.GetKeyDown(KeyCode.J))
-            PlaySound(10);
-        if (Input.GetKeyDown(KeyCode.M))
-            PlaySound(11);
-        if (Input.GetKeyDown(KeyCode.K))
-            PlaySound(12);
-        if (Input.GetKeyDown(KeyCode.Comma))
-            PlaySound(13);
-        if (Input.GetKeyDown(KeyCode.Period))
-            PlaySound(14);
+            PlaySound(0);
+        if (Input.GetKeyDown(KeyCode.U))
+            PlaySound(-1);
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            PlaySound(-2);
+        if (Input.GetKeyDown(KeyCode.Y))
+            PlaySound(-3);
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            PlaySound(-4);
+        if (Input.GetKeyDown(KeyCode.T))
+            PlaySound(-5);
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            PlaySound(-6);
+        if (Input.GetKeyDown(KeyCode.R))
+            PlaySound(-7);
+        if (Input.GetKeyDown(KeyCode.E))
+            PlaySound(-8);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            PlaySound(-9);
+        if (Input.GetKeyDown(KeyCode.W))
+            PlaySound(-10);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            PlaySound(-11);
+        if (Input.GetKeyDown(KeyCode.Q))
+            PlaySound(-12);
+
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
